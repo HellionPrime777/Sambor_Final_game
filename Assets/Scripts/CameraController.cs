ecbdcc6a20af816fb200561a8d3c2222483fe1c4
HellionPrime777 <osambor@ukr.net>
2023-06-25 00:49:23 +0300

    camera, round text

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 30f;
+
+    public float scrollSpeed = 5f;
+
+    public float minY = 20f;
+    public float maxY = 95f;
+
+    private Vector3 startPosOfCam = new Vector3(38.6f, 78.4f, -21.6f);
+    private Vector3 returnBackPosOfCam = new Vector3(38.6f, 78.4f, -21.6f);
+
+    #region
+    //    код відповідає за управління камерою в грі.Основні функції, які він виконує:
+
+    //Визначає швидкість переміщення камери(panSpeed) і швидкість прокрутки(scrollSpeed).
+    //Визначає мінімальну(minY) і максимальну(maxY) висоту, на яку можна підняти або опустити камеру.
+    //Зберігає початкову позицію камери(startPosOfCam) і позицію для повернення(returnBackPosOfCam).
+    //В методі "Update" перевіряє, чи гра завершилася.Якщо так, то вимикає керування камерою.
+    //Обробляє натискання клавіш для переміщення камери вперед, назад, вліво та вправо.
+    //Обробляє натискання клавіші для повернення камери до початкової позиції(CamOrigin).
+    //Обробляє натискання клавіші для повернення камери до останньої збереженої позиції(CamOrigin M).
+    //Обробляє прокрутку колесика миші для зміни висоти камери.
+    //Обмежує висоту камери у заданих межах (minY та maxY).
+    //В загальному, цей клас CameraController забезпечує можливість рухати камеру в грі вперед, назад, вліво, вправо, змінювати її висоту і повертатися до початкової або попередньо збереженої позиції камери.
+    #endregion
+
+    void Update()
+    {
+        if(GameManager.gameIsOver)
+        {
+            this.enabled = false;
+            return;
+        }
+
+        //вперед
+        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        //Назад
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+
+
+        //ВЛІВО
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+        //Вправо
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        //[CamOrigin]
+        //Поверніться до початкового положення камери
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            returnBackPosOfCam = transform.position;
+            transform.position = startPosOfCam;
+        }
+
+        //Повернутися до останньої збереженої позиції камери, коли використовується [CamOrigin] (за замовчуванням = вихідна позиція)
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            transform.position = returnBackPosOfCam;
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 800 * scrollSpeed * Time.deltaTime;
+
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/RoundsSurvived.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+#region
+//код відповідає за анімацію відображення кількості пройдених раундів.
+
+//Основні функції коду включають наступне:
+
+//Задається змінна roundsText, яка представляє текстове поле, в якому відображатиметься кількість пройдених раундів.
+//В методі OnEnable() виконується початок корутини AnimateText(). Це дозволяє почати анімацію при включенні цього скрипту.
+//У корутині AnimateText() спочатку встановлюється текстове поле roundsText в значення "0".
+//Після чеку на протязі 0.7 секунди, починається цикл, який змінює значення roundsText від 0 до значення PlayerStats.rounds.
+//Кожного кроку циклу значення roundsText збільшується на одиницю і відображається на екрані.
+//При цьому цикл чекає 0.055 секунди перед кожним оновленням значення.
+//Коли значення roundsText досягає PlayerStats.rounds, анімація завершується.
+//Отже, цей код створює ефект анімації збільшення числа пройдених раундів на екрані протягом певного часу.
+#endregion
+
+public class RoundsSurvived : MonoBehaviour
+{
+    public Text roundsText;
+
+    private void OnEnable()
+    {
+        StartCoroutine(AnimateText());
+    }
+
+    IEnumerator AnimateText()
+    {
+        roundsText.text = "0";
+        int round = 0;
+
+        yield return new WaitForSeconds(0.7f);
+
+        while(round < PlayerStats.rounds)
+        {
+            round++;
+            roundsText.text = round.ToString();
+            yield return new WaitForSeconds(0.055f);
+        }
+    }
+}
